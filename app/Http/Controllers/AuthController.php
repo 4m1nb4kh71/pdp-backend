5c69e47239205d15333f1a7730c374e8d9ce29b7
4m1n <4m1nb4kh71@gmail.com>
2022-04-28 04:10:32 +0000

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Contracts\Support\ValidatedData;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+
+class AuthController extends BaseController
+{
+    //
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            
+            'email' => 'required|email',
+            'password' => 'required',
+            
+        ]);
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+            $auth = Auth::user(); 
+            $success['token'] =  $auth->createToken('LaravelSanctumAuth')->plainTextToken; 
+            $success['name'] =  $auth->name;
+   
+            return $this->handleResponse($success, 'User logged-in!');
+        } 
+        else{ 
+            return $this->handleError('Unauthorised.', ['error'=>'Unauthorised']);
+        } 
+    }
+
+    public function register(Request $request){
+         $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+        
+        
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('LaravelSanctumAuth')->plainTextToken;
+        $success['name'] =  $user->name;
+   
+        return $this->handleResponse($success, 'User successfully registered!');
+    }
+}
