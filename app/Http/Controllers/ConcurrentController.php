bd33027f5d9e1da1fef069c97a321b4748b807ec
ouiam288289 <98669186+ouiam288289@users.noreply.github.com>
2022-05-18 01:27:42 +0200

    create concurrent

---
+++ app/Http/Controllers/ConcurrentController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Concurrent;
+use App\Http\Requests\StoreConcurrentRequest;
+use App\Http\Requests\UpdateConcurrentRequest;
+use App\Http\Resources\ConcurrentResource;
+
+class ConcurrentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $concurrents = Concurrent::all();
+        return $concurrents;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreConcurrentRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreConcurrentRequest $request)
+    {
+        //
+        $concurrent = Concurrent::create([
+            'viabilite_id'=>$request->viabilite_id,
+         
+            'nom'=>$request->nom,
+      
+            ]);
+        return new ConcurrentResource($concurrent);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Concurrent  $concurrent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Concurrent $concurrent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Concurrent  $concurrent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Concurrent $concurrent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateConcurrentRequest  $request
+     * @param  \App\Models\Concurrent  $concurrent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateConcurrentRequest $request, Concurrent $concurrent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Concurrent  $concurrent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Concurrent $concurrent)
+    {
+        //
+    }
+}
