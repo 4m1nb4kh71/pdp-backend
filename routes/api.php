f98d598b95fa6632774c42d0109e4047d0e9caac
adnaneder <derouich.adnane0@gmail.com>
2022-05-20 17:52:25 +0100

    Fix:update potential filters

--- routes/api.php
+++ routes/api.php
@@ -53,6 +53,8 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::get('/projet_filters',[ProjetController::class,'filters']);
     //solution filters 
     Route::get('/solution_filters',[SolutionController::class,'filters']);
+    //Potentiel filters
+    Route::get('/potentiel_filters',[PotentielController::class,'filters']);
     //projets crud
     Route::get('/projets',[ProjetController::class,'index']);
     Route::post('/create_projet',[ProjetController::class,'store']);
@@ -174,4 +176,3 @@ Route::group(['middleware' => ['auth:api']], function () {
            Route::get('/chiffreaffaires',[ChiffreAffaireController::class,'index']);
            Route::post('/create_chiffreaffaires',[ChiffreAffaireController::class,'store']);
 });
-
