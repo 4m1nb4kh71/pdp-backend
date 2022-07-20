8a7e6ef2c326fc2f0696b3c7e28d3818fe75ff42
AbdessamadSb <sbiliabdessamad12@gmail.com>
2022-07-20 12:47:58 +0100

    update tables

--- database/migrations/2022_05_01_201227_create_associes_table.php
+++ database/migrations/2022_05_01_201227_create_associes_table.php
@@ -16,14 +16,14 @@ return new class extends Migration
         Schema::create('associes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('projet_id')->nullable()->constrained('projets')->onDelete('cascade');
-            $table->text('nom')->default('associe name')->nullable();
-            $table->text('prenom')->default('associe prenom')->nullable();
+            $table->text('nom')->default('associe name')->change()->nullable();
+            $table->text('prenom')->default('associe prenom')->change()->nullable();
             $table->date('date_naissance')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->foreignId('expduration_id')->nullable()->default(1)->constrained('expdurations');
-            $table->text('expfield')->default('associe name')->nullable();
-            $table->text('type')->default('Proteur de projet')->nullable();
+            $table->text('expfield')->default('associe name')->change()->nullable();
+            $table->text('type')->default('Proteur de projet')->change()->nullable();
 
-            $table->text('formation')->default('formation')->nullable();
+            $table->text('formation')->default('formation')->change()->nullable();
             $table->timestamps();
         });
     }
