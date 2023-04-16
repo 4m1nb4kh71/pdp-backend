f4046b8a31a764436cb09cce3c9a4d0618509d00
AbdessamadSb <sbiliabdessamad12@gmail.com>
2023-04-16 15:15:33 +0000

    fix

--- database/migrations/2022_05_01_041651_create_projets_table.php
+++ database/migrations/2022_05_01_041651_create_projets_table.php
@@ -17,22 +17,22 @@ return new class extends Migration
             $table->id();
 
             $table->longText('title');
-            $table->longText('description')->default('projet description')->change()->nullable();
+            $table->longText('description')->nullable();
             $table->date('creation_date')->default(now());
-            $table->string('id_forsa')->default('id forsa')->change();
-            $table->string('scalenote')->default('0')->change()->nullable();
-            $table->string('croissancenote')->default('0')->change()->nullable();
-            $table->string('emplacementnote')->default('0')->change()->nullable();
-            $table->text('secteur')->default('secteur')->change();
-            $table->text('lien')->default('lien')->change()->nullable();
-            $table->longText('commentaire')->default('')->change()->nullable();
-            
+            $table->string('id_forsa')->nullable();
+            $table->string('scalenote')->nullable();
+            $table->string('croissancenote')->default('0')->nullable();
+            $table->string('emplacementnote')->default('0')->nullable();
+            $table->text('secteur')->nullable();
+            $table->text('lien')->nullable();
+            $table->longText('commentaire')->nullable();
 
-           // $table->foreign('secteur_id')->references('id')->on('secteurs');
-        
+
+            // $table->foreign('secteur_id')->references('id')->on('secteurs');
+
 
             $table->foreignId('formejuridique_id')->nullable()->default(1)->constrained('formejuridiques');
-           
+
             $table->timestamps();
         });
     }
--- database/migrations/2022_05_01_201227_create_associes_table.php
+++ database/migrations/2022_05_01_201227_create_associes_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -16,14 +17,14 @@ return new class extends Migration
         Schema::create('associes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('projet_id')->nullable()->constrained('projets')->onDelete('cascade');
-            $table->text('nom')->default('associe name')->change()->nullable();
-            $table->text('prenom')->default('associe prenom')->change()->nullable();
-            $table->date('date_naissance')->default(DB::raw('CURRENT_TIMESTAMP'))->change();
+            $table->text('nom')->nullable();
+            $table->text('prenom')->nullable();
+            $table->date('date_naissance')->nullable();
             $table->foreignId('expduration_id')->nullable()->default(1)->constrained('expdurations');
-            $table->text('expfield')->default('associe name')->change()->nullable();
-            $table->text('type')->default('Proteur de projet')->change()->nullable();
+            $table->text('expfield')->nullable();
+            $table->text('type')->nullable();
 
-            $table->text('formation')->default('formation')->change()->nullable();
+            $table->text('formation')->nullable();
             $table->timestamps();
         });
     }
